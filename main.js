3b26b206f6fcb0f80972c06723035d4ab1c195cc
flapheadxgaming <flapheadx.gaming@gmail.com>
2026-03-01 16:42:02 +0100

    Create main.js file that initializes all core, system, ui, widget, and app modules

---
+++ main.js
@@ -0,0 +1,23 @@
+// main.js
+
+// Initialize Core Modules
+import core from './core';
+
+// Initialize System Modules
+import system from './system';
+
+// Initialize UI Modules
+import ui from './ui';
+
+// Initialize Widget Modules
+import widget from './widget';
+
+// Initialize App Modules
+import app from './app';
+
+// Execute Initializations
+core.init();
+system.init();
+ui.init();
+widget.init();
+app.init();
